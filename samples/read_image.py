844c30c892a582efd3cdf9b0b822b9222a304d83
cac-berkeley <yang@eecs.berkeley.edu>
2024-07-12 12:21:34 -0700

    Added some comments about image format

--- samples/read_image.py
+++ samples/read_image.py
@@ -22,7 +22,7 @@ print('Image shape is: ', data.shape)
 plot_data = data.copy()
 for width in range(512):
     for height in range(10):
-        plot_data[height][width] = [255, 0, 0]
+        plot_data[height][width] = [255, 0, 0]   # Alternatively plot_data[height][width][:] = [255, 0, 0]
         plot_data[511-height][width] = [0,0,255]
 
 # Write the modified images
